c4f303b030a906534347e68155acef2eab43e83d
Thorsten Claff <claff@aiccm.de>
2013-05-23 14:55:59 +0200

    modified:   tm2cry.py

--- tm2cry.py
+++ tm2cry.py
@@ -10,6 +10,15 @@ amSymToInt = {
   "g" : 4
 }
 
+amSymToIntCrystal = {
+  "s" : 0,
+  "sp": 1,
+  "p" : 2,
+  "d" : 3,
+  "f" : 4,
+  "g" : 5
+}
+
 aufbau = {
    1: "s",                   
    2: "s",  3: "p",          
@@ -21,6 +30,8 @@ aufbau = {
   20: "s", 24: "p",
   25: "s"
 }
+
+amOrder = ["s", "p", "d", "f", "g"]
 
 def getMaxNumberEl(sym):
   """ returns the maximal allowed number of electrons
@@ -40,7 +51,7 @@ def readTMToDict(pathToFile):
     """
     # read the actual basis function
     for line in inFileHandle:
-      items = line.split()
+      items = line.strip().split()
 
       if items[0][0] == "#": continue # skip comments
       if items == []: 
@@ -52,44 +63,70 @@ def readTMToDict(pathToFile):
     
       functype = items[1].strip()
       function = { "type"    : functype, 
-                   "numbers" : []      ,
+                   "exp"     : []      ,
+                   "coeff"   : []      ,
                    "numEl"   : 0       ,}
 
       numLinesToRead = int(items[0])
       for line in inFileHandle:
         items = line.split()
-        function["numbers"].append( (float(items[0]), float(items[1])) )
+        function["exp"].append( float(items[0]) )
+        function["coeff"].append( float(items[1]) )
         numLinesToRead -= 1
         if numLinesToRead == 0: break
       
       basisset[symbol]["basis"].append(function)
 
-  def readECP(inFileHandle, basisset):
+  def readECP(inFileHandle, basisset, symbol):
     for line in inFileHandle:
-      items = line.split()
+      items = line.strip()
+      if items[0] == "#": continue # skip comments
+      
+      # if we found this we can break
+      if items[0:5] == "ncore":        
+        items = items.replace("=", " ").split()
+        basisset[symbol]["ecp_ncore"] = items[1]
+        basisset[symbol]["ecp_lmax"]  = items[3]
+        break 
 
-      if items[0][0] == "#": continue # skip comments
+      items = line.split()
+      if items == []: 
+        print "WARNING: Empty line in ecp atomic block!"
+        continue
+
+
+    # found the shell part.. read that mofo    
+    ecp = None
+    for line in inFileHandle:
+      items = line.strip().split()
+      
       if items == []: 
         print "WARNING: Empty line in ecp atomic block!"
         continue
       if items[0].strip()[0] == "*": 
         print "finished ecp for ", symbol
         break
-    
-      functype = items[1].strip()
-      ecp = { "type"    : functype, 
-              "numbers" : []      ,
-              "numEl"   : 0       ,}
+      if items[0] == "#": continue  # skip comments
+      if items[0][0] == "*" : break # this is the end
 
-      numLinesToRead = int(items[0])
-      for line in inFileHandle:
-        items = line.split()
-        function["numbers"].append( (float(items[0]), float(items[1])) )
-        numLinesToRead -= 1
-        if numLinesToRead == 0: break
-      
-      basisset[symbol]["basis"].append(function)
+      # found new shell thingy .. write and  build new ecp 
+      if len(items) == 1:
+        if ecp is not None : basisset[symbol]["ecp"].append(ecp)
+        functype = items[0].strip()
+        ecp = { "type"    : functype, 
+                "exp"     : []      ,
+                "coeff"   : []      ,
+                "r^n"     : []      }
+        continue
+
+      ecp["exp"].append(items[0])
+      ecp["coeff"].append(items[2])
+      ecp["r^n"].append(items[1])
+
+    # the forgotten one
+    if ecp is not None: basisset[symbol]["ecp"].append(ecp)
     
+    print symbol, basisset[symbol]["ecp"]
 
   #############################################################################
   ##                                                                         ##
@@ -102,6 +139,7 @@ def readTMToDict(pathToFile):
   print "Read all basissets from", pathToFile
 
   inFileHandle = open(pathToFile, "r")
+
   for line in inFileHandle:
   
     items = line.split()
@@ -113,8 +151,13 @@ def readTMToDict(pathToFile):
     if items[0].title() not in atoms.chemical_symbols: continue
 
     symbol = items[0].title()
-    basisset[symbol] = {"basis" : [],
-                        "ecp"   : []}
+    
+    # if not existent creat the key
+    if symbol not in basisset.keys():
+      basisset[symbol] = {"basis"     : [],
+                          "ecp"       : [],
+                          "ecp_ncore" : 0 ,
+                          "ecp_lmax"  : 0 }
     
     # check if we got an ecp case
     isECP = False 
@@ -127,6 +170,7 @@ def readTMToDict(pathToFile):
     # there should be a '*' next.. skip comments
     for line in inFileHandle:
       # skip comments
+      if line.strip() == "" : continue
       if line.strip()[0] == "#": continue
       # found the right smybol
       if line.strip()[0] == "*": break
@@ -134,10 +178,11 @@ def readTMToDict(pathToFile):
     if not isECP:
       readBasis(inFileHandle, basisset)
     else:
-      readECP(inFileHandle, basisset)
+      readECP(inFileHandle, basisset, symbol)
 
   inFileHandle.close()
   return basisset
+
 
 def writeAsCrystal(basisset):
   """ Writes the data given in the basiset
@@ -151,7 +196,10 @@ def writeAsCrystal(basisset):
     fileH = open(filename, "w")
     # writing the header to the file
     funcs = basisset[key]["basis"]
-    fileH.write("%d %d\n" % (atoms.atomic_numbers[key], len(funcs)))
+    ecps  = basisset[key]["ecp"]
+    number = atoms.atomic_numbers[key]
+    if ecps != []: number += 200
+    fileH.write("%d %d\n" % (number, len(funcs)))
 
     # distribute the electrons with the aufbau principle
     numUndistributedEl = atoms.atomic_numbers[key]
@@ -171,17 +219,43 @@ def writeAsCrystal(basisset):
           shell["numEl"] = numEl
           break
       counter += 1
+    
+    if ecps != []:
+      fileH.write("INPUT\n")
+
+      # get ecp information 
+      nECP = [0]*6
+      for ecp in ecps:
+        if ecp["type"][0] == "s": nECP[1] += len(ecp["exp"])
+        if ecp["type"][0] == "p": nECP[2] += len(ecp["exp"])
+        if ecp["type"][0] == "d": nECP[3] += len(ecp["exp"])
+        if ecp["type"][0] == "f": nECP[4] += len(ecp["exp"])
+        if ecp["type"][0] == "g": nECP[5] += len(ecp["exp"])
+
+      fileH.write("%d. %d %d %d %d %d %d\n" % 
+                  (number-200-int(basisset[key]["ecp_ncore"]),
+                  nECP[0],nECP[1],nECP[2],nECP[3],nECP[4],nECP[5]
+                  )
+                 )
+
+      for sym in amOrder:
+        for ecp in ecps:
+          if ecp["type"][0] == sym:
+            for coeff, exp, rn in zip(ecp["coeff"],ecp["exp"],ecp["r^n"]):
+              fileH.write("  %4.6f   % 4.6f   %d\n" % 
+                          (float(coeff),float(exp),int(rn))
+                         )
 
 
     for func in funcs:
       fileH.write("0 %d %d %.1f 1.0\n" % 
-                   (amSymToInt[func["type"].lower()],
-                    len(func["numbers"]),
+                   (amSymToIntCrystal[func["type"].lower()],
+                    len(func["exp"]),
                     func["numEl"]
                    )
                  )
-      for prim in func["numbers"]:
-        fileH.write("  %f   %f\n" % prim)
+      for exp, coeff in zip(func["exp"],func["coeff"]):
+        fileH.write("  %f   %f\n" % (exp, coeff))
 
     fileH.close()
 
